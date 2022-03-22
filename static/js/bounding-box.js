9673a434a53af8722c0c58ac492bf2e079f88745
Senuri Wijenayake <senuri.wijenayake@gmail.com>
2022-03-22 18:50:36 +1100

    bounding box

--- static/js/bounding-box.js
+++ static/js/bounding-box.js
@@ -5,7 +5,8 @@ function boundingBox(){
     var instructions_block = {
         type: "html-button-response",
         stimulus: "<p><strong>Instructions</strong></p>" +
-        "<p>Instructions for Entity Recognition Task</p>",
+        "<p>In this task you will use the bounding box tool to draw boxes around each instance of the requested target of interest." +
+        "<br/>More detailed instructions will be available under the <b>Instructions</b> tab in next page.</p>",
         choices: ['Continue'],
         data: {trial_category: 'instructions', unique_trial_id: 1190},
         on_load: function () {
@@ -14,32 +15,34 @@ function boundingBox(){
             script.type = 'text/javascript';
             script.src = 'https://assets.crowd.aws/crowd-html-elements.js';
             head.appendChild(script);
+
+            var custom_script = document.createElement('script');
+            custom_script.type = 'text/javascript';
+            custom_script.src = '/static/js/crowd-html-scripts.js';
+            head.appendChild(custom_script);
+
         }
     };
 
     timeline.push(instructions_block);
 
-    timeline.push({
-        type: "external-html",
-        url: "boundingbox.html",
-        execute_script: true,
-        on_load: function(){
-            var head = document.getElementsByTagName('head')[0];
-            var script = document.createElement('script');
-            script.type = 'text/javascript';
-            script.src = '/static/js/crowd-html-scripts.js';
-            head.appendChild(script);
-        },
-        cont_btn: "aws-submit",
-        check_fn: function(){
-            var data = JSON.parse(sessionStorage.getItem("boundingBox"));
-            return true;
-        },
-        on_finish: function(data){
-            data.tags = JSON.parse(sessionStorage.getItem("boundingBox"));
-        }
-    });
+    var randomIndex = jsPsych.randomization.shuffle([1,2,3]);
 
+    for (let i in randomIndex){
+        timeline.push({
+            type: "external-html",
+            url: "boundingbox/boundingbox" + randomIndex[i] + ".html",
+            execute_script: true,
+            cont_btn: "aws-submit",
+            check_fn: function(){
+                var data = JSON.parse(sessionStorage.getItem("boundingBox"));
+                return true;
+            },
+            on_finish: function(data){
+                data.tags = JSON.parse(sessionStorage.getItem("boundingBox"));
+            }
+        });
+    }
 
     return timeline;
 }
--- static/js/crowd-html-scripts.js
+++ static/js/crowd-html-scripts.js
@@ -1,8 +1,9 @@
-document.querySelector('crowd-form').onsubmit = function(e) {
-  e.preventDefault();
-  const boundingBoxes = document.querySelector('crowd-bounding-box').value.boundingBoxes || document.querySelector('crowd-bounding-box')._submittableValue.boundingBoxes;
-  console.log(boundingBoxes);
-  sessionStorage.setItem("boundingBox", JSON.stringify(boundingBoxes));
-  $('#aws-submit').trigger('click');
-}
-
+setInterval(function() {
+    if(document.querySelector("crowd-form")){
+      document.querySelector("crowd-form").addEventListener("submit", function(e){
+          e.preventDefault();
+          const boundingBoxes = document.querySelector('crowd-bounding-box').value.boundingBoxes || document.querySelector('crowd-bounding-box')._submittableValue.boundingBoxes;
+          sessionStorage.setItem("boundingBox", JSON.stringify(boundingBoxes));
+          $('#aws-submit').trigger('click');
+        });
+    }}, 500);
