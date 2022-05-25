2d9dc195031bb819563fcea0965617d257bc8eeb
Senuri Wijenayake <senuri.wijenayake@gmail.com>
2022-05-25 20:27:51 +1000

    adding NER tasks

--- static/js/full-flow.js
+++ static/js/full-flow.js
@@ -7,11 +7,11 @@ var timeline = [];
 var welcome_block = {
     type: "html-button-response",
     stimulus: "<p><strong>Welcome</strong></p>" +
-    "<p>You will complete 14 tasks. This includes a series of tests that assess your cognitive and comprehension skills,</p> " +
+    "<p>You will complete 14 tasks. This includes a series of tests that assess your cognitive and comprehension skills, " +
     "<p>personality, alertness and mood, followed by a set of crowdsourcing tasks.</p>" +
-    "<p>Please ensure that you complete all 14 tasks in a single sitting to receive payment. </p>" +
-    "<p><strong>You will not receive any payment if you do not complete all tasks.</strong></p>" +
-    "<p><strong>Read the instructions carefully </strong>before starting each task. </p> <p>Instructions will not be repeated.</p> ",
+    "<p>Please ensure that you complete all 14 tasks in a <u><b>single sitting</b></u> to receive payment. <br/> <strong>You will not receive any payment if you do not complete all tasks.</strong></p>" +
+    "<p style ='color:red'><b>Do NOT refresh the browser window until you complete all tasks. <br/>Refreshing the browser will result in a data loss and you will not receive any payment.</b></p>" +
+    "<p><strong>Read the instructions carefully </strong>before starting each task. Instructions will not be repeated.</p> ",
     choices: ['Continue']
 };
 
@@ -20,11 +20,12 @@ timeline.push(welcome_block);
 // Setting up tests
 
 var cognitiveTests  = [stroop(), flanker(), nBack(), taskSwitching(), pointing()];
-var allTests = [alertness(), comprehension(), mood(), personality(), cognitiveTests];
+var allTests = [entityAnnotation()];
 
+// alertness(), comprehension(), mood(), personality(), cognitiveTests
 
 // Populate timeline with tests
-var randomIndex = jsPsych.randomization.shuffle([0,1,2,3,4]);
+var randomIndex = jsPsych.randomization.shuffle([0]);
 var taskNumber = 1;
 
 for (let t in randomIndex){
@@ -65,47 +66,47 @@ for (let t in randomIndex){
 // Setting up tasks
 
 
-var crowdsourcingTasks = [boundingBox(), entityAnnotation(), classification(), sentiment(), transcription()];
-
-randomIndex = jsPsych.randomization.shuffle([0,1,2,3,4]);
-
-for (let t in randomIndex){
-    var taskNumber = t*1+10;
-    timeline.push({
-        type: "html-button-response",
-        stimulus: "<p><strong>Task "+ taskNumber +
-        "</strong></p><p>&nbsp;</p>",
-        choices: ['Continue'],
-        on_load: function () {
-            window.scroll(0,0);
-        }
-    });
-    timeline = timeline.concat(crowdsourcingTasks[randomIndex[t]]);
-}
-
-timeline.push({
-    type: "html-button-response",
-    stimulus: "<p><strong>You have completed all the tasks." +
-    "</strong></p><p>Click the button below to answer a short questionnaire on your context and demographics.</p>",
-    choices: ['Continue'],
-    button_htm: ['<button class="jspsych-btn">%choice%</button>'],
-    on_load: function () {
-        window.scroll(0,0);
-    }
-});
-
-timeline = timeline.concat(questionnaire());
-
-timeline.push({
-    type: "html-button-response",
-    stimulus: "<p><strong>Thank You!" +
-    "</strong></p><p>Click the button below to return to Mechanical Turk to submit your answers.</p>",
-    choices: ['Complete'],
-    button_htm: ['<button class="jspsych-btn">%choice%</button>'],
-    on_load: function () {
-        jsPsych.setProgressBar(1);
-    }
-});
+//var crowdsourcingTasks = [boundingBox(), entityAnnotation(), classification(), sentiment(), transcription()];
+//
+//randomIndex = jsPsych.randomization.shuffle([0,1,2,3,4]);
+//
+//for (let t in randomIndex){
+//    var taskNumber = t*1+10;
+//    timeline.push({
+//        type: "html-button-response",
+//        stimulus: "<p><strong>Task "+ taskNumber +
+//        "</strong></p><p>&nbsp;</p>",
+//        choices: ['Continue'],
+//        on_load: function () {
+//            window.scroll(0,0);
+//        }
+//    });
+//    timeline = timeline.concat(crowdsourcingTasks[randomIndex[t]]);
+//}
+//
+//timeline.push({
+//    type: "html-button-response",
+//    stimulus: "<p><strong>You have completed all the tasks." +
+//    "</strong></p><p>Click the button below to answer a short questionnaire on your context and demographics.</p>",
+//    choices: ['Continue'],
+//    button_htm: ['<button class="jspsych-btn">%choice%</button>'],
+//    on_load: function () {
+//        window.scroll(0,0);
+//    }
+//});
+//
+//timeline = timeline.concat(questionnaire());
+//
+//timeline.push({
+//    type: "html-button-response",
+//    stimulus: "<p><strong>Thank You!" +
+//    "</strong></p><p>Click the button below to return to Mechanical Turk to submit your answers.</p>",
+//    choices: ['Complete'],
+//    button_htm: ['<button class="jspsych-btn">%choice%</button>'],
+//    on_load: function () {
+//        jsPsych.setProgressBar(1);
+//    }
+//});
 
 jsPsych.init({
     timeline: timeline,
